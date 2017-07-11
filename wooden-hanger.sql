21731f7bf0eb7d6f67e3d2f864b15c7a5cde01ff
mcdollano <mcdollano@gmail.com>
2017-07-11 16:47:46 +0800

    Added updated sql

--- wooden-hanger.sql
+++ wooden-hanger.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 10, 2017 at 10:58 AM
+-- Generation Time: Jul 11, 2017 at 10:41 AM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 7.1.2
 
@@ -43,7 +43,17 @@ INSERT INTO `guest_customers` (`guest_id`, `guest_firstname`, `guest_lastname`, 
 (1, '', '', 0, '', 'Quezon City'),
 (2, '', '', 0, '', 'naga city'),
 (3, '', '', 0, '', 'Legazpi City'),
-(4, '', '', 0, '', 'Naga City');
+(4, '', '', 0, '', 'Naga City'),
+(5, 'swaggy', 'swaggy', 1234567, 'swaggy', 'quezon city'),
+(6, 'mark', 'dollano', 123456789, 'ajsodjaksdj', 'quezon city'),
+(7, 'markmark', 'dollano', 123456789, 'asdjhajshd', 'quezon city'),
+(8, 'hahahaha', 'hahahaha', 123456789, 'jahsdjsad', 'quezon city'),
+(9, 'hasjdhjahsd', 'jhajshdjahs', 123456789, 'jahsjdhajshd', 'quezon city'),
+(10, 'jhjashd', 'ajkhdakjhs', 123456789, 'jashdjkahd', 'quezon city'),
+(11, 'ajsdhkjadhskjahs', 'asjhdaksjhdasd', 12456789, 'ajsdhjkahds', 'quezon city'),
+(12, 'mark', 'dollano', 1231321, 'jashdkjahdas', 'quezon city'),
+(13, 'jhjkhsdkf', 'ajksdhkajshda', 12312312, 'akjhdkajsa', 'asjkdhaskjdh'),
+(14, '', '', 0, '', '');
 
 -- --------------------------------------------------------
 
@@ -116,7 +126,17 @@ INSERT INTO `orders` (`order_id`, `user_id`, `guest_id`, `order_date`, `required
 (33, NULL, NULL, '2017-07-10', NULL, NULL, NULL, 'Quezon City', 10500),
 (34, NULL, NULL, '2017-12-25', NULL, NULL, NULL, 'naga city', 17500),
 (35, NULL, NULL, '2017-07-10', NULL, NULL, NULL, 'Legazpi City', 7000),
-(36, NULL, NULL, '2017-12-25', NULL, NULL, NULL, 'Naga City', 14000);
+(36, NULL, NULL, '2017-12-25', NULL, NULL, NULL, 'Naga City', 14000),
+(37, NULL, NULL, '2017-10-05', NULL, NULL, NULL, 'quezon city', 18000),
+(38, NULL, NULL, '2017-10-05', NULL, NULL, NULL, 'quezon city', 15000),
+(39, NULL, NULL, '0000-00-00', NULL, NULL, NULL, 'quezon city', 13000),
+(40, NULL, NULL, '2017-10-05', NULL, NULL, NULL, 'quezon city', 10500),
+(41, NULL, NULL, '0000-00-00', NULL, NULL, NULL, 'quezon city', 16500),
+(42, NULL, NULL, '2017-10-05', NULL, NULL, NULL, 'quezon city', 3000),
+(43, NULL, NULL, '2017-12-25', NULL, NULL, NULL, 'quezon city', 6000),
+(44, NULL, NULL, '2017-12-24', NULL, NULL, NULL, 'quezon city', 10500),
+(45, NULL, NULL, '0000-00-00', NULL, NULL, NULL, 'asjkdhaskjdh', 7000),
+(46, NULL, NULL, '0000-00-00', NULL, NULL, NULL, '', 14000);
 
 -- --------------------------------------------------------
 
@@ -132,6 +152,18 @@ CREATE TABLE `order_details` (
   `item_price` int(11) NOT NULL,
   `sub_total` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `order_details`
+--
+
+INSERT INTO `order_details` (`order_details_id`, `order_id`, `item_id`, `order_quantity`, `item_price`, `sub_total`) VALUES
+(5, 40, 29, 3, 3500, 10500),
+(6, 41, 29, 2, 3000, 6000),
+(7, 42, 30, 1, 3000, 3000),
+(8, 43, 30, 2, 3000, 6000),
+(9, 44, 29, 3, 3500, 10500),
+(10, 45, 29, 2, 3500, 7000);
 
 -- --------------------------------------------------------
 
@@ -213,7 +245,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `guest_customers`
 --
 ALTER TABLE `guest_customers`
-  MODIFY `guest_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `guest_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT for table `items`
 --
@@ -223,12 +255,12 @@ ALTER TABLE `items`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 --
 -- AUTO_INCREMENT for table `order_details`
 --
 ALTER TABLE `order_details`
-  MODIFY `order_details_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `order_details_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 --
 -- AUTO_INCREMENT for table `users`
 --
